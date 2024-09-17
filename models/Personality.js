c05b9cbfeba45665e4f2e0589cf50d68b79e78c3
sajjadhaider <sajjadhaider@creatoros.co>
2024-09-17 22:15:07 +0530

    few changes

--- models/Personality.js
+++ models/Personality.js
@@ -1,11 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const PersonalitySchema = new mongoose.Schema({
-  token: String,
-  email: String,
-  embeddings: [Number],
-  serviceType: String,
-  bothServicesObtained: { type: Boolean, default: false }
-}, { timestamps: true });
+const PersonalitySchema = new mongoose.Schema(
+  {
+    token: String,
+    email: { type: String, default: null },
+    embeddings: [Number],
+    serviceType: String,
+    bothServicesObtained: { type: Boolean, default: false },
+    displayName: { type: String, default: null },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Personality', PersonalitySchema);
+module.exports = mongoose.model("Personality", PersonalitySchema);
